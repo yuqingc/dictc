b024fa5a201a76f1c6508231c646a199fd95e857
Matt <mr.chenyuqing@live.com>
2018-07-16 22:50:08 +0800

    chore(*): npm init & publish a npm module
    v0.0.0 for `not prepared for release`

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dictc
 Generate a manual for your React component lib
+
+Not prepared for release at present
---
+++ index.js
@@ -0,0 +1,6 @@
+// Update later
+// Not prepared for release
+
+const dictc = 'hello dictc';
+
+exports.dictc = dictc;
